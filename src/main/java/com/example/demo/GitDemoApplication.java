a9bda433009c08c53e3b040fa128a73eaf5958e8
Admin <Admin@DESKTOP-BD24KSJ>
2019-09-16 11:39:35 +0530

    hii

--- src/main/java/com/example/demo/GitDemoApplication.java
+++ src/main/java/com/example/demo/GitDemoApplication.java
@@ -9,6 +9,7 @@ public class GitDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 		System.out.println("hiii");
+		System.out.println("hii ihello");
 	}
 
 }
